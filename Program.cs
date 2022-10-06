7da62f89a244982618f91d30fe0772ee9c5f4c12
Folasayo Samuel <folasayosamuel@gmail.com>
2022-10-06 20:22:14 +0100

    update

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GenerateTheSumOfNNunbers
+{
+	public class Program
+	{
+		public static void Main(string[]args)
+		{
+			int number, sum = 0;
+
+			Console.Write("Enter a Number: ");
+			number = Convert.ToInt32(Console.ReadLine());
+			
+			if(number < 0)
+				Console.Write("Please Enter Positive Number");
+			else
+				while(number>0)
+				{
+					sum += number;
+					number -= 1;
+				}
+			Console.WriteLine("The sum is "+sum);
+
+            Console.ReadKey();
+        }
+	}
+}
